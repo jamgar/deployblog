1f8d5d056f802f8599ae070fa444c1267def661f
github username <github email address>
2018-11-14 21:22:33 -0600

    Deploy message

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,6 +31,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :branch,        :master
 # set :format,        :pretty
 # set :log_level,     :debug
+#
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-2.5.0'
