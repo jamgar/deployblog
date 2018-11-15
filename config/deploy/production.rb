3b5b28503e13da50c97729e3cb8685e5272a3b33
github username <github email address>
2018-11-14 18:38:13 -0600

    Change deploy.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '174.129.78.121', user: 'deploy', roles: %w{web app db}
+# server '174.129.78.121', user: 'deploy', roles: %w{web app db}
 
 
 # role-based syntax
